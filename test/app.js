d18f7fc684238f68cbf38fe45b031336e3fce64f
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2016-10-31 17:15:45 +0000

    Mejora el estilo del metodo _publish.

--- test/app.js
+++ test/app.js
@@ -1,8 +1,29 @@
-var hologger    = require("../index")(require("./logger.json"))
-;
+//lectura de variables de entorno
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+
+
+//importacion
+var hologger    = require("../index")(require("./logger.json"));
 
 
 
 //realizo los test
-hologger.debug(1, {member: "pekebuda"});
+hologger.debug(1, "foo");
 hologger.info(1, "foo");
+hologger.notice(1, "foo");
+hologger.warning(1, "foo");
+hologger.error(1, "foo");
+hologger.critical(1, "foo");
+hologger.alert(1, "foo");
+hologger.emergency(1, "foo");
+
+hologger.debug(2, {member: "debug"});
+hologger.info(2, {member: "info"});
+hologger.notice(2, {member: "notice"});
+hologger.warning(2, {member: "warning"});
+hologger.error(2, {member: "error"});
+hologger.critical(2, {member: "critical"});
+hologger.alert(2, {member: "alert"});
+hologger.emergency(2, {member: "emergency"});
