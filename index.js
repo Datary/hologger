16eb5f4f92b84dca494589c7c44e5c8a4fb81f6e
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-11-01 12:14:58 +0100

    Modifica module.exports: se exporta un singleton.

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 /**
- * @description
- * Debido al analisis en cascada de las variables de entorno, la existencia 
- * de una pretiere la otra. 
- *
- * 
- * @param {Object} library: 
- * @param {Number} isil: identificador numerico de la libreria de codigos 
- * empleada
+ * Module dependencies.
  */
-var Logger 	= require("./lib/logger");
+var Hologger = require("./lib/logger");
 
 
-module.exports = function(library, isil){
-    //@TODO sanity checks
-    isil = isil || null;
-    library = library || {};
-    process.env = process.env || {};
-    var strategy = (process.env.LOGENTRIES_TOKEN)?
-        "Logentries" : (process.env.APPINSIGHTS_INSTRUMENTATIONKEY)?
-            "ApplicationInsights": "Local";
-    
-    return new Logger(strategy, library, isil);
-};
+/**
+ * Export default singleton.
+ *
+ * @api public
+ */
+exports = module.exports = new Hologger();
+
+
+/**
+ * Expose constructors.
+ */
+exports.Hologger =
+exports.Logger = Hologger;
+
+
+/**
+ * Expose strategies.
+ */
+exports.strategies = {};
